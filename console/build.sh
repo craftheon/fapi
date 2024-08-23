0e11a2214f55ceb17502265c6029e5ab3fb737f9
yuangwei <yuang.wya@gmail.com>
2024-08-23 10:28:49 +0800

    Update console build script

---
+++ console/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+set -x
+process_name=$1
+
+function compile() {
+  CGO_ENABLED=0
+  GOOS=linux
+  GOARCH=amd64
+  go mod tidy
+  go build -o main ./cmd/main.go
+}
+
+function make_output() {
+  (
+    compile &&
+      rm -rf dist &&
+      mkdir -p dist &&
+      cp ./main dist
+      rm -rf ./main
+  ) || {
+    echo "[make output error]"
+    exit 1
+  }
+}
+
+make_output
+
+echo "[build done!]"
+exit 0
